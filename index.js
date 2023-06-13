d20526e6a477b420e1413b7b5412e9f679a2feba
az-ad <tasmim.cu.cse567@gmail.com>
2023-06-13 14:11:08 +0600

    null

--- index.js
+++ index.js
@@ -27,59 +27,147 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //     client.close();
 // });
 
-async function run() {
-    try {
-        const commentCollection = client.db("azad-fruit-shop").collection('comments');
-        // const comment = { name: 'mahi', content: 'very good' }
-        // const result = await commentCollection.insertOne(comment);
-        // console.log(result);
-        app.get('/comment', async (req, res) => {
-            const cursor = commentCollection.find({});
-            const comments = await cursor.toArray();
-            res.send(comments);
-        });
-        app.post('/comment', async (req, res) => {
-            // console.log('post api called');
-            const comment = req.body;
-            //const result = await commentCollection.insertOne(comment);
-            //comment.name = comments.length + 1;
-            // comments.push(comment);
-            // console.log(req.body);
-            //console.log(comment);
-            const result = await commentCollection.insertOne(comment);
-            console.log(result);
-            comment._id = result.insertedId;
+// async function run() {
+//     try {
+//         const commentCollection = client.db("azad-fruit-shop").collection('comments');
+//         app.get('/comment', async (req, res) => {
+//             const cursor = commentCollection.find({});
+//             const comments = await cursor.toArray();
+//             res.send(comments);
+//         });
+//         app.post('/comment', async (req, res) => {
+//             console.log('post api called');
+//             const comment = req.body;
+//             const result = await commentCollection.insertOne(comment);
+//             console.log(result);
+//             comment._id = result.insertedId;
 
-            res.send(comment);
-        });
+//             res.send(comment);
+//         });
 
-    }
-    finally {
+//     }
+//     finally {
 
-    }
-}
-run().catch(err => console.log(err))
+//     }
+// }
+// run().catch(err => console.log(err))
 
 async function run() {
     try {
         const serviceCollection = client.db("azad-fruit-shop").collection('services');
+        const commentCollection = client.db("azad-fruit-shop").collection('comments');
+        const userCollection = client.db("azad-fruit-shop").collection('users');
+        const postCollection = client.db("azad-fruit-shop").collection('posts');
+        const ratingCollection = client.db("azad-fruit-shop").collection('rating');
 
+
+        //------------------services-----------------------------------//
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
             const services = await cursor.toArray();
             res.send(services);
         });
 
+        //------------------------------comment---------------------//
+        app.get('/comment', async (req, res) => {
+            const cursor = commentCollection.find({});
+            const comments = await cursor.toArray();
+            res.send(comments);
+        });
+        app.post('/comment', async (req, res) => {
+            console.log('post api called');
+            const comment = req.body;
+            const result = await commentCollection.insertOne(comment);
+            console.log(result);
+            comment._id = result.insertedId;
+
+            res.send(comment);
+        });
+        //----------------------------------all user----------------------//  
+
+        app.get('/user', async (req, res) => {
+            const cursor = userCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+        app.post('/user', async (req, res) => {
+            // console.log('post api called');
+            const user = req.body;
+
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            user._id = result.insertedId;
+
+            res.send(user);
+        });
+
+        //----------------------------------posts-----------------------//
+        app.get('/post', async (req, res) => {
+            const cursor = postCollection.find({});
+            const posts = await cursor.toArray();
+            res.send(posts);
+        });
+        app.post('/post', async (req, res) => {
+            // console.log('post api called');
+            const post = req.body;
+
+            const result = await postCollection.insertOne(post);
+            console.log(result);
+            post._id = result.insertedId;
+
+            res.send(post);
+        });
+
+        //-------------rating----------------------------------------------//
+        app.get('/rating', async (req, res) => {
+            const cursor = ratingCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+        app.post('/rating', async (req, res) => {
+            // console.log('post api called');
+            const user = req.body;
+
+            const result = await ratingCollection.insertOne(user);
+            console.log(result);
+            user._id = result.insertedId;
+
+            res.send(user);
+        });
 
     }
     finally {
 
     }
 }
-
-
 run().catch(err => console.log(err))
 
+// async function run() {
+//     try {
+//         const userCollection = client.db("azad-fruit-shop").collection('users');
+
+//         // app.get('/user', async (req, res) => {
+//         //     const cursor = userCollection.find({});
+//         //     const users = await cursor.toArray();
+//         //     res.send(users);
+//         // });
+//         app.post('/user', async (req, res) => {
+//             // console.log('post api called');
+//             const user = req.body;
+
+//             const result = await userCollection.insertOne(user);
+//             console.log(result);
+//             user._id = result.insertedId;
+
+//             res.send(user);
+//         });
+
+//     }
+//     finally {
+
+//     }
+// }
+// run().catch(err => console.log(err))
 
 app.listen(port, () => {
     console.log(`simple node running on port ${port}`);
